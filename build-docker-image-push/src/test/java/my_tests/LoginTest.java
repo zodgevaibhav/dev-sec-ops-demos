c568e7db4145b66fc39fa6d79bf07c6f6ef53eb7
Vaibhav Zodge <vzodge@dnyanyog.org>
2024-04-27 18:30:04 +0530

    feat(docker-image): Build & Publish image to registry
    * Build docker image using
          spring-boot-maven-plugin
    * Publish image to docker registry
    * No need of docker file

---
+++ build-docker-image-push/src/test/java/my_tests/LoginTest.java
@@ -0,0 +1,14 @@
+//package my_tests;
+//
+//import org.junit.Test;
+//
+//public class LoginTest {
+//
+//  @Test
+//  public void testFirstTest() throws InterruptedException {
+//    for (int i = 0; i < 1000; i++) {
+//      System.out.println("Running test " + i);
+//      Thread.sleep(5);
+//    }
+//  }
+//}
